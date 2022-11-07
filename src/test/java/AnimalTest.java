5bc9269691dac68e1fa0fa275136b18d861636cb
n.polyakova <natli.d26.l8@gmail.com>
2022-11-07 20:27:58 +0300

    Add new test

--- src/test/java/AnimalTest.java
+++ src/test/java/AnimalTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class AnimalTest {
 
     @Test
@@ -19,4 +21,10 @@ public class AnimalTest {
 
         Assertions.assertEquals(animal.age, 3);
     }
+
+    @Test
+    public void shouldDoNothing() {
+        assertTrue(true);
+    }
+
 }
